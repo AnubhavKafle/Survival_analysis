258aff294876493b72b4fe4c2f03c227d343e0ef
Anubhav Kaphle <anubhavkaphle@gmail.com>
2017-02-03 12:39:15 +0100

    Finding_significant_genes_per_tumor
    This is used to filter genes based on their p-values (< 0.05) and hazard ratio (>1).

---
+++ Finding_significant_genes_per_tumor.R
@@ -0,0 +1,23 @@
+cox_pvalues = read.table("TCGA_Cox_Pval_Allgenes.txt", sep= "\t", header = T, row.names = 1)
+
+cox_hazardRatio = read.table("TCGA_Cox_Hazardratio.txt", sep="\t", header = T , row.names = 1)
+
+cox_pvalues[!is.finite(as.matrix(cox_pvalues))] <- 0
+
+cox_hazardRatio[!is.finite(as.matrix(cox_hazardRatio))] <- 0
+
+genes_significantP_greaterHazard = list()
+
+for ( i in 1:length(colnames(cox_hazardRatio)))
+{
+  for(j in 1:length(rownames(cox_hazardRatio)))
+  {
+    if ( cox_pvalues[j,i] <= 0.05 & cox_hazardRatio[j,i] >= 1)
+    {
+      genes_significantP_greaterHazard[[rownames(cox_pvalues)[j]]] = c(cox_pvalues[j,i], cox_hazardRatio[j,i])
+    }
+  }
+  significant_genes = as.matrix(genes_significantP_greaterHazard[[]][2])
+  
+  write.table(significant_genes, file = paste(colnames(cox_hazardRatio)[i],".txt"), sep = "\t")
+}
