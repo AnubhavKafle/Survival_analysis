23f272422a6b7c855258a98d623f8c8f78a876ff
Anubhav Kaphle <anubhavkaphle@gmail.com>
2017-02-06 15:34:21 +0100

    Update Survival_mutationData.R

--- Survival_mutationData.R
+++ Survival_mutationData.R
@@ -1,11 +1,12 @@
-###### This is to format the file suitable for Surivival analysis.########
-for i in $(ls *.txt) ; do awk -F'\t' '{print ($1,$9,$16) }' $i | grep -v -E "Silent|RNA" | sed 's/ /\t/g'  >> "Filtered_"$i; done 
-for i in $(ls Filtered*) ; do  cut -d"-" -f1,2,3,4 $i >> "filtered_"$i; done
+###### This is to format the file suitable for Surivival analysis.Done in BASH SHELL ########
+for i in $(ls *.txt) ; do awk -F'\t' '{print ($1,$9,tolower($16)) }' $i | grep -v -E "Silent|RNA" | sed 's/ /\t/g'  >> "Filtered_"$i; done 
+for i in $(ls Filtered*) ; do  cut -d"-" -f1,2,3 $i >> "filtered_"$i; done
 rm Filtered*
 mv filtered* ./Mu
 rename 's/filtered_Filtered_/Filtered_/' filtered*
 for i in $(ls *.txt) ; do  sed 's/Tumor_Sample_Barcode/Patience_ID/g' $i >> "tested_"$i; done
 rm Filtered*
+cd Mu/
 rename 's/tested_//g' tested_Filtered_*
 
 ########################################
@@ -26,9 +27,46 @@ for ( i in names(genes_acc))
   
   mutation_matrix[[i]] = cbind(rep(1,length(genes_acc[[i]])))
   rownames(mutation_matrix[[i]]) = as.character(genes_acc[[i]])
-  
+   colnames(mutation_matrix[[i]])= "Status"
 }
 
+  clinical_data = t(read.table(list.files("~/Desktop/labRotation1_AnubhavK/Clinical_Firehose/",pattern="ACC",full.names = T),header=T,sep="\t",row.names = 1))[,-1]
+
+clinical_match = as.data.frame(clinical_data)
+
+event = c() #To have all the events coded in 0 or 1
+
+days_to_death = c() #days to death, if not, then follow-up data
+
+##Preparing clinical survival data###
+for ( i in 1:length(as.character(clinical_match$days_to_death))) #Take follow-day where death days are not available
+{
+  days_to_death[i] = ifelse(is.na(as.character(clinical_match$days_to_death)[i]),as.numeric(as.character(clinical_match$days_to_last_followup))[i],as.numeric(as.character(clinical_match$days_to_death))[i])
+  
+  event[i] = as.numeric(as.character(clinical_match$vital_status))[i]
+}
+
+clinical_survival = as.data.frame(cbind(days_to_death,event))
+
+rownames(clinical_survival)= rownames(clinical_match)
+
+####################
+survival_mutation_matrix = list()
+
+status = c()
+
+for(j in names(genes_acc))
+{
+  
+  for(i in rownames(clinical_survival))
+  {
+    status = ifelse( i %in% genes_acc[[j]][], 1, 0) 
+  }
+  
+  survival_mutation_matrix[[j]] = cbind(status)
+  rownames(survival_mutation_matrix[[j]]) = rownames(clinical_survival)
+}
+  
 
 
 
